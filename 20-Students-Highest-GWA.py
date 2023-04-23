b4b2e97f5aa5f542ceb84069ad3e52ab8347ee4b
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-23 16:48:38 +0800

    initial commit

---
+++ 20-Students-Highest-GWA.py
@@ -0,0 +1,6 @@
+# Joshua Lemuel Z. Centina BS CPE 1-4
+# 20 Students Highest GWA
+
+# Write a Python program that read a file containing the name of 20 students together with their GWA. 
+# The program will outputs the name of the student who got the highest GWA (including the GWA).
+
