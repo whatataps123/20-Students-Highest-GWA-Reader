52af67020e8ca9bb5d51cd5ae1e24cabd07c78af
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-23 22:16:49 +0800

    def student and open studentgwa.txt

--- 20-Students-Highest-GWA.py
+++ 20-Students-Highest-GWA.py
@@ -4,7 +4,10 @@
 # Write a Python program that read a file containing the name of 20 students together with their GWA. 
 # The program will outputs the name of the student who got the highest GWA (including the GWA).
 
+def student():
 # open studentgwa.txt (read)
+    with open("studentgwa.txt", "r") as input_gwa:
 # read the file line by line
 # find the min gwa(lesser is better) of the student
 # print name of student and gwa
+student()
